30eaf779614ec66c193f453c4a2098ba0b24525f
Anton Sterner <anton.sterner@gmail.com>
2017-11-10 17:12:39 +0100

    small changes

--- ImageRotation.m
+++ ImageRotation.m
@@ -1,6 +1,5 @@
-function RImage = ImageRotation(OImage, center, degangle )
+function RImage = ImageRotation(OImage, center, degangle, point1, point2)
 %FRotate Rotate an image around a given point
-%   Everybody has to use this template
 %
 %% Who has done it
 %
@@ -17,36 +16,6 @@ function RImage = ImageRotation(OImage, center, degangle )
 %
 % Output arguments: RImage is the rotated image
 %
-% You MUST NEVER change the first line
-%
-%% Basic version control (in case you need more than one attempt)
-%
-% Version: 2
-% Date: 6/11/2017
-%
-% Gives a history of your submission to Lisam.
-% Version and date for this function have to be updated before each
-% submission to Lisam (in case you need more than one attempt)
-%
-%% General rules
-%
-% 1) Don't change the structure of the template by removing %% lines
-%
-% 2) Document what you are doing using comments
-%
-% 3) Before submitting make the code readable by using automatic indentation
-%       ctrl-a / ctrl-i
-%
-% 4) In case a task requires that you have to submit more than one function
-%       save every function in a single file and collect all of them in a
-%       zip-archive and upload that to Lisam. NO RAR, NO GZ, ONLY ZIP!
-%       All non-zip archives will be rejected automatically
-%
-% 5) Often you must do something else between the given commands in the
-%       template
-%
-%
-
 %% Information about the image (size, type etc)
 %       You can assume that it has uint8 pixels 
 %       What should you do if this is not the case?
@@ -58,7 +27,7 @@ function RImage = ImageRotation(OImage, center, degangle )
 %   coordinate vector of the same pixel)
 %
 ir = [1:sr];%index vector for pixels along first Matlab dimension
-ic = [1:sc];%same in the second direction
+ic = [1:sc];%index vector for pixels in the second direction
 
 % NOTE: centerpoint is picked with ginput, so the x & y coords. are
 % switched
@@ -75,13 +44,11 @@ cic = ic - center(1,1);%Same for the second axis
 
 %% Convert the degress, modify the angles and 
 %   transform back to Euclidean coordinates 
-%   you may skip the next two lines and modify the input to pol2cart
-%   if you want
 
-rads = degangle*pi/180;% degs...
+rads = degangle*pi/180;% radians...
 
 % clockwise rotion of mesh coords with desired angle
-TNew = Theta-rads;% use Theta and degs
+TNew = Theta-rads;% use Theta and rads
 
 [nC,nR] = pol2cart(TNew, Rho); %
 
@@ -97,21 +64,20 @@ newic = nC + center(1,1);
 %% Now use nearest neighbor interpolation (round) and rotate
 
  RImage = uint8(zeros(sr,sc,nc));
-% 
-% 
 
 for k = 1:sr
     for l = 1:sc
         % setting pixels ouside image range to black
         if(newir(k,l) < 1 || newir(k,l) > sr)
-            RImage(k,l) = 0;
+            RImage(k,l,:) = 0;
        
         elseif(newic(k,l) < 1 || newic(k,l) > sc)
-            RImage(k,l) = 0;
+            RImage(k,l,:) = 0;
        
             % Filling RImage with new "rotated" pixel values
         else
-            RImage(k,l) = OImage(uint32(round(newir(k,l))),uint32(round(newic(k,l))));
+            %RImage = interplmg(OImage, [newir(k,l), newic(k,l)], true);
+            RImage(k,l,:) = OImage(uint32(round(newir(k,l))),uint32(round(newic(k,l))));
         end
         
     end
--- face_orientation.m
+++ face_orientation.m
@@ -3,16 +3,24 @@
 
 % facial orientation adjustment, image and eye positions is input, returns scaled
 % and rotated image
-function [ rotated ] = face_orientation( image, point1, point2 )
+function [ rotatedimg ] = face_orientation( image, point1, point2 )
 
 % mid point
 midpoint = (point1 + point2)/2;
+
+
 % angle between eyes
 angle = -rad2deg(atan2(point2(2) - point1(2), point2(1) - point1(1))); 
 
 % rotate image, works with grayscale images
-rotated = ImageRotation(image, midpoint, angle);
+% rotates image 'angle' degrees around the mid point, i.e. between the eyes
+% should result in an image with 
+rotatedimg = ImageRotation(image, point1, angle, point1, point2);
+%imshow(rotatedimg);
+%line([point1(1) point2(1)], [point1(2) point2(2)])
 
+% show image with lines between eyes (TO ADD: and mouth)
+%facetriangle(rotatedimg, point1, point2, midpoint);
 % scale image 
 % ADD CODE 
 
