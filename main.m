acb8cf51150ddebedf6569859a0af800c514cddf
tobma571 <tobma571@student.liu.se>
2017-11-27 16:50:30 +0100

    Main update

--- main.m
+++ main.m
@@ -17,7 +17,7 @@ clear;
 close all;
 
 % Read image from database
-input = imread('images/DB1/db1_10.jpg');
+input = imread('images/DB1/db1_05.jpg');
 
 % Color correct image
 output = colorCorrection(input);
@@ -27,10 +27,32 @@ figure
 imshow(output);
 title('Color corrected');
 
-% Face masking
-facemask = detectFace(output);
+faceMask = detectFace(output);
+figure
+imshow(faceMask);
+
+%Eye positions
+[eyePos1, eyePos2] = eyeMap(output, faceMask);
+
+%Mouth map and mouth position
+[mouthmap, mouthPos] = mouthMap(output);
+
+rotatedImage = face_orientation(output, eyePos1, eyePos2);
+
+%Crop image
+eyeCenter = round((eyePos1 + eyePos2)./2);
+centerOfImage = round((eyeCenter + mouthPos)./2);
+
+xmin = centerOfImage(1) - 125;
+xmax = centerOfImage(1) + 125;
+ymin = centerOfImage(2) - 200;
+ymax = centerOfImage(2) + 150;
+
+width = xmax- xmin;
+height = ymax - ymin;
+
+    
+cropped = imcrop(rotatedImage,[xmin ymin width height]);
 
 figure
-imshow(facemask);
-title('Face mask with morphing')
-
+imshow(cropped)
