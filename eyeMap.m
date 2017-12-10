f22e92bd4de47b6e6e099ecc368fe80d7ea2e3c0
tobma571 <tobma571@student.liu.se>
2017-12-10 18:00:07 +0100

    Added comments to eyeMap

--- eyeMap.m
+++ eyeMap.m
@@ -2,55 +2,74 @@ function [eye1, eye2, numberOfEyes] = eyeMap(Im, mouthPos)
    
     image = im2uint8(Im);
     
+    % Convert to YCbCr color space and extract the components
     ycbcrmap = rgb2ycbcr(image);
-
     Y = ycbcrmap(:,:,1);
     Cb = ycbcrmap(:,:,2);
     Cr = ycbcrmap(:,:,3);
-
+    
+    % Calculate Cb^2
     dCb = double(Cb);
     CbPow = power(dCb,2);
-
+    
+    % Normalize Cb^2 to range between 0 and 255
     normCbPow = CbPow./max(max(CbPow));
     CbPow = normCbPow.*255;
 
+    % Calculate the negative of Cr
     CrNeg = 255 - Cr;
+    
+    % Calculate CrNeg^2
     dCrNeg = double(CrNeg);
     CrNegPow = power(dCrNeg,2);
-
+    
+    % Normalize the negative of Cr between 0 and 255
     normCrNegPow = CrNegPow./max(max(CrNegPow));
     CrNegPow = normCrNegPow.*255;
 
+    % Calculate Cb/Cr
     CbDivCr = double(Cb)./double(Cr);
 
+    % Normalize Cb/Cr between 0 and 255
     normCbDCr = CbDivCr./max(max(CbDivCr));
     CbDCrFinal = normCbDCr.*255;
 
+    % Calculate eyeMapC
     eyeMapC = (1/3).*CbPow + (1/3).*CrNegPow + (1/3).*CbDCrFinal;
 
+    % Histogram equalization to enhance contrast
     eyeMapC = histeq(uint8(eyeMapC));
 
+    % Structuring element for dilation and erosion
     SE = strel('sphere',20);
 
+    % Erosion of the luma component
     erosion = imerode(Y,SE);
     erosion = erosion + 1;
+    
+    % Dilation of the luma component
     dilation = imdilate(Y,SE);
 
+    % Calculating EyeMapL
     eyeMapL = double(dilation)./double(erosion);
 
+    % Normalizing EyeMapL between 0 and 255 
     eyeMapLN = eyeMapL./max(max(eyeMapL));
-
     eyeMapLFinal = uint8(eyeMapLN.*255);
 
+    % Fusing EyeMapC and EyeMapL to create the final eye map
     eyeMapFinal = imfuse(eyeMapLFinal, eyeMapC, 'blend');
 
+    % Dilating the final eye map
     SE2 = strel('sphere',15);
     eyeMapFinal = imdilate(eyeMapFinal, SE2);
 
+    % Normalizing the final eye map between 0 and 255
     eyeMapFinal = 255.*double(eyeMapFinal)./max(max(double(eyeMapFinal)));
 
     [rows, cols] = size(eyeMapFinal);
 
+    % Thresholding the map to create map with intensities of only 0 and 255
     for row = 1:rows
         for col = 1:cols
             if(eyeMapFinal(row,col) > 201)
@@ -60,7 +79,8 @@ function [eye1, eye2, numberOfEyes] = eyeMap(Im, mouthPos)
             end
         end
     end
-
+    
+    % Makes the map binary
     BW = logical(eyeMapFinal);
     
     % Cropping eyemap 
@@ -78,10 +98,12 @@ function [eye1, eye2, numberOfEyes] = eyeMap(Im, mouthPos)
     
     mm = bwareafilt(eyemap,2); % Selecting 2 largest object of image
 
+    % Calculates the center of each object
     labeledImage = bwlabel(mm);
     measurements = regionprops(labeledImage, mm, 'Centroid');
     [a, b] = size(measurements);
     
+    % If two centriods are found, they become the eye positions 
     if(a == 2)
         centroid1 = measurements(1).Centroid;
         centroid2 = measurements(2).Centroid;
@@ -96,16 +118,16 @@ function [eye1, eye2, numberOfEyes] = eyeMap(Im, mouthPos)
         eye2 = centroid2;
         numberOfEyes = 2;
         return
-    else
-        if(a == 1)
-            eye1 = [];
-            eye2 = [];
+    else 
+        if(a == 1) 
+            eye1 = [];              % If there is only one centroid found, eye positions are not set 
+            eye2 = [];              % and number of eyes are set to one.
             numberOfEyes = 1;
             return
         else
-            eye1 = [];
-            eye2 = [];
-            numberOfEyes = 0;
+            eye1 = [];              % If there is less than one centroid, eye positions are are also not set
+            eye2 = [];              % and number of eyes are set to zero
+            numberOfEyes = 0;        
             return
         end
 
